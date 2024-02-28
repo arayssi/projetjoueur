6c67b300bc338e3cbe6be21c32c31d01bab5ce4e
arayssi <araysimohamedali@gmail.com>
2024-02-28 14:55:57 +0100

    checkpoint props

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PlayersList from "./composant/PlayersList";
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/composant/players.js
@@ -0,0 +1,23 @@
+const players = [
+    {
+      name: "Mpapper",
+      team: "Paris Saint-Germain",
+      nationality: "ASome Nationalit",
+      jerseyNumber: 99,
+      age: 28,
+     imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwjgu1K37TIuaO76_HSKm2ds11E6Wjco7fafDovXXelh4cEIfixSesWA8YiSGjoKWMajk&usqp=CAU ",
+
+    },
+
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portuguese",
+      jerseyNumber: 7,
+      age: 37,
+      imageUrl:
+        "https://th.bing.com/th/id/OIF.MQ9hTJqicZHjzsyImFg6PA?rs=1&pid=ImgDetMain",
+    },
+  ];
+  
+  export default players;
